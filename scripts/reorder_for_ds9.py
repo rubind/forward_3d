343fbb4ca5f4adef0bbfa99ba9357e5189ec6d4f
David Rubin <you@example.com>
2017-02-01 16:26:36 -0800

    reorder

--- scripts/reorder_for_ds9.py
+++ scripts/reorder_for_ds9.py
@@ -2,18 +2,19 @@ import pyfits
 from numpy import *
 import sys
 
-newim = sys.argv[1].replace(".fits", "_ds9.fits")
-assert newim != sys.argv[1]
-
-f = pyfits.open(sys.argv[1])
-dat = f[0].data
-
-
-print dat.shape
-dat = transpose(dat, [0,3,1,2])
-print dat.shape
-
-f[0].data = dat
-f.writeto(newim, clobber = "True")
-f.close()
+for i in range(1, len(sys.argv)):
+    newim = sys.argv[i].replace(".fits", "_ds9.fits")
+    assert newim != sys.argv[i]
+    
+    f = pyfits.open(sys.argv[i])
+    dat = f[0].data
+    
+    
+    print dat.shape
+    dat = transpose(dat, [0,3,1,2])
+    print dat.shape
+    
+    f[0].data = dat
+    f.writeto(newim, clobber = "True")
+    f.close()
 
