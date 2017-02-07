e5a155a53021b69a3f2b22065656d983087397f6
David Rubin <davidrubinlbl@gmail.com>
2017-02-06 22:10:43 -0500

    div

--- scripts/div.py
+++ scripts/div.py
@@ -3,12 +3,30 @@ from numpy import *
 import sys
 from DavidsNM import save_img
 
-f = pyfits.open(sys.argv[1])
-dat = f[0].data
-f.close()
+if " ".join(sys.argv).count(".fits"):
 
-f = pyfits.open(sys.argv[2])
-dat2 = f[0].data
-f.close()
+    f = pyfits.open(sys.argv[1])
+    dat = f[0].data
+    f.close()
 
-save_img(dat/dat2, "div.fits")
+    f = pyfits.open(sys.argv[2])
+    dat2 = f[0].data
+    f.close()
+
+    save_img(dat/dat2, "div.fits")
+
+else:
+    
+    for dr in sys.argv[1:]:
+        f = pyfits.open(dr + "/resid_ds9.fits")
+        dat = f[0].data
+        f.close()
+        
+        f = pyfits.open(dr + "/true_scene_ds9.fits")
+        dat2 = f[0].data
+        f.close()
+
+        save_img(dat/dat2, dr + "/div.fits")
+        
+
+
