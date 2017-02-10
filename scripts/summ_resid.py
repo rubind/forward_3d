6d020f2fa2c11bd072b52ffab64e1eeeb87ca00a
rubind <davidrubinlbl@gmail.com>
2017-02-09 17:14:38 -0800

    summ

--- scripts/summ_resid.py
+++ scripts/summ_resid.py
@@ -1,35 +1,42 @@
 import pyfits
 from numpy import *
+import sys
 
-f = pyfits.open("resid.fits")
-dat = f[0].data
-f.close()
+if len(sys.argv) == 1:
+    wds = ["."]
+else:
+    wds = sys.argv[1:]
 
-f = pyfits.open("true_scene.fits")
-dat2 = f[0].data
-f.close()
-
-frac_resid = dat/dat2
-inds = where(1  - isnan(frac_resid))
-frac_resid = frac_resid[inds]
-
-print std(frac_resid)
-print 1.4826*median(abs(frac_resid - median(frac_resid)))
-
-
-
-
-
-
-for dith in range(dat.shape[0]):
-
-    frac_resid = abs(dat[dith])/(abs(dat2[dith]).max())
+for wd in wds:
+    f = pyfits.open(wd + "/resid.fits")
+    dat = f[0].data
+    f.close()
     
-    print "max", dith, abs(dat2[dith]).max()
-    print "max", dith, abs(dat[dith]).max(), "=", abs(dat[dith]).max()/abs(dat2[dith]).max()
+    f = pyfits.open(wd + "/true_scene.fits")
+    dat2 = f[0].data
+    f.close()
     
+    frac_resid = dat/dat2
     inds = where(1  - isnan(frac_resid))
     frac_resid = frac_resid[inds]
     
-    print dith, std(frac_resid)
-    print dith, 1.4826*median(abs(frac_resid - median(frac_resid)))
+    print std(frac_resid)
+    print 1.4826*median(abs(frac_resid - median(frac_resid)))
+
+
+
+
+
+
+    for dith in range(dat.shape[0]):
+        
+        frac_resid = abs(dat[dith])/(abs(dat2[dith]).max())
+        
+        print "max", dith, abs(dat2[dith]).max()
+        print wd, "max", dith, abs(dat[dith]).max(), "=", abs(dat[dith]).max()/abs(dat2[dith]).max()
+        
+        inds = where(1  - isnan(frac_resid))
+        frac_resid = frac_resid[inds]
+        
+        print dith, std(frac_resid)
+        print dith, 1.4826*median(abs(frac_resid - median(frac_resid)))
